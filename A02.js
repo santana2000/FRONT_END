00ca0b276573b9be6da39bc1660e7d4b30836b55
santana2000 <34099487+santana2000@users.noreply.github.com>
2018-03-16 16:31:10 +0800

    Add files via upload

---
+++ A02.js
@@ -0,0 +1,40 @@
+'use strict';
+alert('c404');//测试
+/*
+块注释
+*/
+NaN; // NOT A NUMBER
+1.234e5; //* 10^5
+
+//==   '自动转换类型再比较'
+//===  '类型不同直接false'  <right>
+
+''; //长度为0的字符串
+var arr=[a,b,c,1,2,null,false];
+
+var c404= {          //对象用花括号
+    tutor: 'sun',    //键值对  键——属性
+    student: '123',
+    table: 'ture'
+};
+console.log(c404.student);
+
+var s; // $ 或者 _ 开头，不能以数字开头
+//只能声明一次，可反复赋值（不同类型的值也可以）
+s; //不通过var声明代表全局变量  strict模式
+
+//ES6新特性
+`多行
+字符
+串表示方法`;
+
+var name='haiwai';
+var age=15;
+var school='cumt';
+var info=`名字是${name},今年${age}岁,在${school}上学`;
+console.log(info);
+
+var o='fjskljfiejw';
+o.substring(2,6)
+
+
---
+++ A03.js
@@ -0,0 +1,40 @@
+//第二天
+var arr=[2,4,2,2];
+
+arr.length=6;
+console.log(arr);
+
+arr.indexOf(2);//String & 数组
+
+arr.slice(1,3); //包括开始，不包括结束索引
+arr.slice(3);
+arr.slice();//复制数组
+
+arr.sort();
+console.log(arr);
+
+arr.reverse();
+
+arr.splice //万能方法   (x,y,'m')  x开始，删除y个
+
+var sum=[[200,400],[300,500],'ss'];
+var m=sum[1][1];
+console.log(m);
+
+var i={
+    name:'y',                    //i.name
+    'middle_school':'yizhong'    //对象属性含有特殊字符时用单引号
+}                                //调用时用i['middle_school']
+                                 //访问不存在的属性i.age   不报错，显示undefined
+var x=2;
+for( ; ; ){  //不写条件 进入死循环
+    x++;
+    if(x>19){
+        break;
+    }
+}
+
+//Map 键值对数据类型
+
+const pi=3.14;
+
