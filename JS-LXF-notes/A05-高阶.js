dd0c4647d7beac1a192f7b9e5fb16906b2d11829
tesla <532014809@qq.com>
2018-09-18 11:40:14 +0800

    review

--- JS-LXF-notes/A05-高阶.js
+++ JS-LXF-notes/A05-高阶.js
@@ -17,14 +17,27 @@ function multiple(a,b) {
     return a+b;
 }
 console.log(f);
+
 var m=qqq.reduce(INT);
 function INT(a,b) {
     return a*10+b;
 }
 console.log(m);
 
-var  r='s';
+//map + reduce 1.实现 string2int 功能，先用split分割字符串
+//             2.转换为首字母大写，注意观察数组中各元素的特点
+var name = ['adam', 'LISA', 'barT']
+
+
+var arr = ['A', '', 'B', null, undefined, 'C', '  '];
+var sss = arr.filter(function (s) {   // filter根据回掉函数的返回值是来判断是否保留该元素
+    return s && s.trim(); // 这里是判断条件
+});
+
+
+var  r='9';
 var  rr=r-0;
 console.log(rr);
 
 //sort() 先把元素转化为字符串再排序
+//设置比较函数进一步细化比较规则
