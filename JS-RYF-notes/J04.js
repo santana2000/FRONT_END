0af20d4a684669317ad4a3cb500a0220482b1119
santana2000 <532014809@qq.com>
2019-03-26 11:14:49 +0800

    cumt home page

--- JS-RYF-notes/J04.js
+++ JS-RYF-notes/J04.js
@@ -1,6 +1,41 @@
+/*
 
+var name = '李四';
 
-var man = 1;
-var man2 = 2;
+var A = {
+    name: '张三',
+    describe: function () {
+        console.log('姓名：'+ this.name) ;
+    }
+};
 
+A.describe();
 
+var f = A.describe;
+
+f();
+*/
+
+// console.log(this === window);
+
+//webstorm中是node环境
+
+var a = [1,2,3,4];
+console.log(Math.max.apply(null,a));
+
+var counter = {
+    count: 0,
+    inc: function () {
+        this.count++;
+    }
+};
+
+var func = counter.inc.bind(counter);
+func();
+
+/*
+var func = counter.inc;
+func.call(counter);
+*/
+
+counter.count
